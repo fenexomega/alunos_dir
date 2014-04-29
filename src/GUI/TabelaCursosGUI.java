24ae198ca20a37a8e4aae151587c986a72a1a46a
Jordy Ferreira <jordyfgomes@gmail.com>
2014-04-29 19:51:55 -0300

    Modificações

--- src/GUI/TabelaCursosGUI.java
+++ src/GUI/TabelaCursosGUI.java
@@ -18,6 +18,11 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 import javax.swing.DefaultComboBoxModel;
 
 public class TabelaCursosGUI
@@ -46,11 +51,57 @@ public class TabelaCursosGUI
 		frmTabelaDeCursos = new JFrame();
 		frmTabelaDeCursos.setTitle("Tabela de Cursos");
 		frmTabelaDeCursos.setBounds(100, 100, 450, 448);
-		frmTabelaDeCursos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTabelaDeCursos.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmTabelaDeCursos.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		frmTabelaDeCursos.addComponentListener(new ComponentListener()
+		{
+			
+			@Override
+			public void componentShown(ComponentEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				comboBox.setModel(new DefaultComboBoxModel(programa.getCursesNames()));
+
+			}
+			
+			@Override
+			public void componentResized(ComponentEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void componentMoved(ComponentEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void componentHidden(ComponentEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(programa.getCursesNames()));
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				alunos = programa.getTabelaAlunos(programa.getCursoByIndex(comboBox.getSelectedIndex()));
+				table.setModel(new DefaultTableModel(
+						alunos,
+						new String[] {
+							"ALUNO"
+						}
+					));
+				
+			}
+		});
 		frmTabelaDeCursos.getContentPane().add(comboBox, BorderLayout.NORTH);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -65,24 +116,7 @@ public class TabelaCursosGUI
 			}
 		));
 		scrollPane.setViewportView(table);
-		
-		JButton btnNewButton = new JButton("Atualizar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
-			{
 				
-				alunos = programa.getTabelaAlunos(programa.getCursoByIndex(comboBox.getSelectedIndex()));
-				table.setModel(new DefaultTableModel(
-						alunos,
-						new String[] {
-							"ALUNO"
-						}
-					));
-				
-				
-			}
-		});
-		frmTabelaDeCursos.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
 	}
 
 }
